e929fd967949a1872784b844e09de93b22b4f3cd
karishev <sk8795@nyu.edu>
2024-06-19 18:08:23 +0400

    feat: added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import StyleContext from "@/components/context/styleContext";
 import { useState } from "react";
 import { ThemeProvider } from "@/components/context/themeContext";
 import { ApplyTheme } from "@/components/atoms/applyTheme";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <StyleContext.Provider value={{ menuOpen, setMenuOpen }}>
       <ThemeProvider>
+        <Analytics />
         <ApplyTheme />
         <Component {...pageProps} />
       </ThemeProvider>
